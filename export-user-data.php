cd6ae4708ab41dd4dd93b4c7f83c6eefe1aef17f
qlstudio <qlstudio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-11 20:03:15 +0000

    WP Support Fixes
    git-svn-id: https://plugins.svn.wordpress.org/export-user-data/trunk@786600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- export-user-data.php
+++ export-user-data.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * @package Export_User_Data
- * @version 0.7.1
+ * @version 0.7.2
  */
 /*
 Plugin Name: Export User Data
 Plugin URI: http://qstudio.us/plugins/
 Description: Export User data, metadata and BuddyPressX Profile data.
-Version: 0.7.1
+Version: 0.7.2
 Author: Q Studio
 Author URI: http://qstudio.us/
 License: GPL2
